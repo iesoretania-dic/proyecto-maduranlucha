98421fd21590270337e067af02271a71ce46ba2a
Miguel Angel Duran Lucha <maduranlucha@gmail.com>
2018-05-13 03:04:31 +0200

    Añadido un acceso directo en el cliente para que el administrador pueda liberarlo del comercial.

---
+++ controller/cliente/cliente_liberar.php
@@ -0,0 +1,74 @@
+<?php
+require '../../php/Consulta.php';
+session_start();
+//var_dump($_POST);
+//var_dump($_SESSION);
+
+if(!isset($_SESSION['usuario'])){
+    header('Location: ../../index.php');
+}elseif($_SESSION['rol'] != '0'){
+    $datos = new Consulta();
+    $datos->set_noautorizado();
+    header('Location: ../login/no_autorizado.php');
+}else{
+
+    $rol = $_SESSION['rol'];
+    $usuario  = $_SESSION['usuario'];
+    $datos = new Consulta();
+    $idUsuario = $datos->get_id();
+    $dni = $_GET['Id']; // Aqui recibimos el dni del cliente o del comercial depende de donde se envie.
+    $tipo = $_GET['tipo']; // 0 para usuario, 1 para cliente
+    $mensajeUpdate = null;
+    echo $tipo;
+
+    //Accion al pulsar el boton de liberar
+    if(isset($_POST['btnLiberar'])){
+
+        if ($tipo == '1'){
+
+            $consulta = "UPDATE cliente SET id_usuario = NULL WHERE dni = :dni";
+            $parametros = array(":dni"=>$dni);
+            $datos = new Consulta();
+            $filasAfectadas = $datos->get_sinDatos($consulta,$parametros);
+
+            if($filasAfectadas > 0){
+                $mensajeUpdate = "ok";
+                header('Location: ../cliente/cliente_listar.php');
+            }else{
+                $mensajeUpdate = "error";
+            }
+
+
+        }
+    }
+
+    //Accion en el caso de pulsar el boton de cancelar
+    if(isset($_POST['btnCancelar'])){
+        //desde el panel del cliente
+        if($tipo == '1'){
+            header('Location: ../cliente/cliente_listar.php');
+        }
+    }
+
+    ////////////////////////Renderizado//////////////////////////
+    require_once '../../vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem('../../views');
+    $twig = new Twig_Environment($loader, []);
+
+    try{
+        echo $twig->render('cliente/cliente_liberar.twig', compact(
+            'usuario',
+            'clientes',
+            'mensajeUpdate',
+            'cliente',
+            'dni',
+            'nombre',
+            'comerciales',
+            'rol',
+            'filasAfectadas',
+            'tipo'
+        ));
+    }catch (Exception $e){
+        echo  'Excepción: ', $e->getMessage(), "\n";
+    }
+}
