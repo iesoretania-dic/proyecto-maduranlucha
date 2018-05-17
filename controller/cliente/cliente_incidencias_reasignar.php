0a40f7cc50b5598198af66181065a4f058f14d12
Miguel Angel Duran Lucha <maduranlucha@gmail.com>
2018-05-17 16:59:26 +0200

    Añadido un boton de accion para poder reasignar las incidencias entre los tecnicos, para los administradores.

---
+++ controller/cliente/cliente_incidencias_reasignar.php
@@ -0,0 +1,109 @@
+<?php
+require '../../php/Consulta.php';
+require '../../php/funciones.php';
+
+session_start();
+//var_dump($_POST);
+//var_dump($_SESSION);
+
+if(!isset($_SESSION['usuario'])){
+    header('Location: ../../index.php');
+}elseif($_SESSION['rol'] != '0' and ($_SESSION['rol'] != '4')){
+    $datos = new Consulta();
+    $datos->set_noautorizado();
+    header('Location: ../login/no_autorizado.php');
+}else{
+
+    $mensaje = null;
+    $nombreTecnico = null;
+    $rol = $_SESSION['rol'];
+    $usuario  = $_SESSION['usuario'];
+    $idIncidencia = $_GET['Id'];
+
+    //primero miramos si esa incidencia no la tiene como principal un tecnico
+    $datos = new Consulta();
+    $consulta = "SELECT nombre FROM usuario WHERE asignada = :idIncidencia";
+    $parametros = array(":idIncidencia"=>$idIncidencia);
+    $principal = $datos->get_conDatosUnica($consulta,$parametros);
+
+    if($principal){
+       $mensaje = 'principal';
+       $nombreTecnico = $principal['nombre'];
+    }else{
+        //Obtenemos una lista de tecnicos disponibles
+        $datos = new Consulta();
+        $consulta = "SELECT dni, nombre FROM usuario WHERE rol = :rol";
+        $parametros = array(":rol"=>'2');
+        $tecnicos = $datos->get_conDatos($consulta,$parametros);
+    }
+
+
+
+
+    if(isset($_POST['btnAceptar'])){
+
+        $nuevoTenico = $_POST['nuevoTecnico'];
+
+        //Volvemos a comprobar si esta como principal por algun tecnico
+        $datos = new Consulta();
+        $consulta = "SELECT nombre FROM usuario WHERE asignada = :idIncidencia";
+        $parametros = array(":idIncidencia"=>$idIncidencia);
+        $nprincipal = $datos->get_conDatosUnica($consulta,$parametros);
+
+        if(!$nprincipal){
+
+            if($nuevoTenico != ''){
+
+                $consulta = "UPDATE incidencia SET tecnico = :tecnico, estado = '2', fecha_inicio = :fecha, llamada_obligatoria = 'No' WHERE id_incidencia = :incidencia";
+                $parametros = array(":tecnico"=>$nuevoTenico,":incidencia"=>$idIncidencia,":fecha"=>date("Y-m-d H:i:s"));
+                $datos = new Consulta();
+                $resultado = $datos->get_sinDatos($consulta,$parametros);
+
+                if ($resultado > 0){
+                    $mensaje = 'ok';
+                    header('Location: ../cliente/cliente_incidencias.php?tipo=0');
+                }else{
+                    $mensaje = 'error';
+                }
+
+
+            }else{
+
+                $consulta = "UPDATE incidencia SET tecnico = :tecnico, fecha_inicio = NULL, estado = '1', llamada_obligatoria = 'No' WHERE id_incidencia = :incidencia";
+                $parametros = array(":tecnico"=>NULL,":incidencia"=>$idIncidencia);
+                $datos = new Consulta();
+                $resultado = $datos->get_sinDatos($consulta,$parametros);
+
+                if ($resultado > 0){
+                    $mensaje = 'ok';
+                    header('Location: ../cliente/cliente_incidencias.php?tipo=0');
+                }else{
+                    $mensaje = 'error';
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+    ////////////////////////Renderizado//////////////////////////
+    require_once '../../vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem('../../views');
+    $twig = new Twig_Environment($loader, []);
+
+    try{
+        echo $twig->render('cliente/cliente_incidencias_reasignar.twig', compact(
+            'mensaje',
+            'comentarios',
+            'rol',
+            'usuario',
+            'tecnicos',
+            'nombreTecnico'
+        ));
+    }catch (Exception $e){
+        echo  'Excepción: ', $e->getMessage(), "\n";
+    }
+}
