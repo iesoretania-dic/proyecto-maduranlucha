87efb23b852e54d88dc7ae2885fc2aac98478e02
Miguel Angel Duran Lucha <maduranlucha@gmail.com>
2018-05-17 17:36:34 +0200

    Añadido un boton de accion para poder ver los comentarios de las incidencias para los administradores, solucionado un problema al modificar los cliente para los administradores.

---
+++ controller/cliente/cliente_incidencias_comentario.php
@@ -0,0 +1,46 @@
+<?php
+require '../../php/Consulta.php';
+session_start();
+//var_dump($_POST);
+//var_dump($_SESSION);
+
+if(!isset($_SESSION['usuario'])){
+    header('Location: ../../index.php');
+}elseif($_SESSION['rol'] != '0' and ($_SESSION['rol'] != '1')){
+    $datos = new Consulta();
+    $datos->set_noautorizado();
+    header('Location: ../login/no_autorizado.php');
+}else{
+
+    $mensaje = null;
+    $rol = $_SESSION['rol'];
+    $usuario  = $_SESSION['usuario'];
+    $idIncidencia =  $_GET['Id'];
+    $consulta = "SELECT * FROM comentarios WHERE id_incidencia = :incidencia ORDER BY fecha";
+    $parametros = array(":incidencia"=> $idIncidencia);
+    $datos = new Consulta();
+    $comentarios = $datos->get_conDatos($consulta,$parametros);
+
+    if($comentarios){
+        $mensaje = 'Ok';
+    }else{
+        $mensaje = 'error';
+    }
+
+
+    ////////////////////////Renderizado//////////////////////////
+    require_once '../../vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem('../../views');
+    $twig = new Twig_Environment($loader, []);
+
+    try{
+        echo $twig->render('cliente/cliente_incidencias_comentario.twig', compact(
+            'mensaje',
+            'usuario',
+            'rol',
+            'comentarios'
+        ));
+    }catch (Exception $e){
+        echo  'Excepción: ', $e->getMessage(), "\n";
+    }
+}
