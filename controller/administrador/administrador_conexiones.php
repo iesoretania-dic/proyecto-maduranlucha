9f6c3722edc140b7fe3659ac75882742a9d68aaa
Miguel Angel Duran Lucha <maduranlucha@gmail.com>
2018-05-23 20:56:15 +0200

    Añadido en la barra de navegacion del administrador la posibilidad de ver un registro de conexiones a la aplicacion.

---
+++ controller/administrador/administrador_conexiones.php
@@ -0,0 +1,73 @@
+<?php
+require '../../php/Consulta.php';
+session_start();
+//var_dump($_POST);
+//var_dump($_SESSION);
+
+if(!isset($_SESSION['usuario'])){
+    header('Location: ../../index.php');
+}elseif($_SESSION['rol'] != '0'){
+    $datos = new Consulta();
+    $datos->set_noautorizado();
+    header('Location: ../../index.php');
+}else{
+
+    $usuario  = $_SESSION['usuario'];
+    $rol = $_SESSION['rol'];
+    $datos = new Consulta();
+    $idUsuario = $datos->get_id();
+
+    $consulta = "SELECT conexiones.fecha, conexiones.usuario,conexiones.tipo, usuario.nombre FROM conexiones INNER JOIN usuario ON conexiones.usuario = usuario.dni ORDER BY conexiones.fecha DESC";
+    $parametros = array();
+    $datos = new Consulta();
+    $conexiones = $datos->get_conDatos($consulta,$parametros);
+
+    if($conexiones){
+        $mensaje = 'ok';
+    }else{
+        $mensaje = 'error';
+    }
+
+    $vista = 'conexiones';
+
+    if(isset($_POST['limpiar'])){
+        $vista = 'limpiar';
+
+    }
+
+    if(isset($_POST['aceptarLimpiar'])){
+
+        $consulta ="DELETE FROM conexiones";
+        $parametros = array();
+        $datos = new Consulta();
+        $filasAfectadas = $datos->get_sinDatos($consulta,$parametros);
+        header("Location: administrador_conexiones.php");
+    }
+
+
+    if(isset($_POST['cancelarLimpiar'])){
+        $vista = 'conexiones';
+
+    }
+
+
+
+    ////////////////////////Renderizado//////////////////////////
+    require_once '../../vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem('../../views');
+    $twig = new Twig_Environment($loader, []);
+
+    try{
+        echo $twig->render('administrador/administrador_conexiones.twig', compact(
+            'usuario',
+            'tecnicos',
+            'mensaje',
+            'rol',
+            'conexiones',
+            'vista'
+
+        ));
+    }catch (Exception $e){
+        echo  'Excepción: ', $e->getMessage(), "\n";
+    }
+}
